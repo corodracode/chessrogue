6de52fe6e26a771f2946c0095acc80c97b2c3eac
Claudio Rodriguez <corodriguezb@gmail.com>
2025-10-21 21:57:28 -0300

    fixed bugs with compound move set

--- compound_move_set.gd
+++ compound_move_set.gd
@@ -3,8 +3,14 @@ extends MoveSet
 
 @export var move_sets: Array[MoveSet]
 
-func get_moves(piece_ui: PieceUI, board: Board):
+func get_moves(piece_ui: PieceUI, board: Board) -> PackedVector2Array:
 	var curr_moves: PackedVector2Array
 	for ms: MoveSet in move_sets:
 		curr_moves.append_array(ms.get_moves(piece_ui, board))
+	curr_moves.append_array(super(piece_ui, board))
 	return curr_moves
+
+func flip_y_moves():
+	super()
+	for move_set: MoveSet in move_sets:
+		move_set.flip_y_moves()
